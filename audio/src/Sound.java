27fc0bfaa96952fc0663e65e48b9e7d75576c276
vmdva <vmdva@192.168.0.20>
2019-09-20 00:05:32 -0400

    Added Instrument Change

--- audio/src/Sound.java
+++ audio/src/Sound.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import javax.sound.midi.*;
 import java.util.*;
-import java.util.Map.Entry;
 
 
 
@@ -22,7 +21,7 @@ public class Sound
 			File file = new File(f_name);
 			FileReader f_reader = new FileReader(file);
 			BufferedReader b_reader = new BufferedReader(f_reader);
-			
+				
 			String row;
 			
 			//col[0] = note #, col[1] = note_name
@@ -31,8 +30,9 @@ public class Sound
 				note_mapper.put(col[1], Integer.parseInt(col[0]));
 				reverse_note_mapper.put(Integer.parseInt(col[0]),col[1]);
 			}
-			
+			f_reader.close();
 			b_reader.close();
+			
 		} catch (Exception e) {e.printStackTrace();}
 
 	}
@@ -47,24 +47,56 @@ public class Sound
 	}
 	
 	
+	public static void change_instrument(Integer channel, Integer bank, Integer patch, Synthesizer synth) {
+		// NEED TO LOAD IF NEEDED
+		try {
+			Receiver rec = synth.getReceiver();
+			
+			//bank is a collection of presets
+			//Receiver.send(MidiMessage message, long timeStamp)
+			//ShortMessage(command, channel, data1,data2); 
+			//program_change(
+			rec.send(new ShortMessage(ShortMessage.CONTROL_CHANGE, channel, 0, bank >> 7), -1);
+			rec.send(new ShortMessage(ShortMessage.CONTROL_CHANGE, channel, 32, bank & 0x7F), -1);
+			rec.send(new ShortMessage(ShortMessage.PROGRAM_CHANGE, channel, patch, 0), -1);
+		} catch (Exception e) {e.printStackTrace();}
+
+	}
+	
 	public static void main(String args[]){
 		
 		Sound test = new Sound();
 		
-		print_dict(test.note_mapper);
+//		print_dict(test.note_mapper);
 
 		try {
 			Synthesizer synth = MidiSystem.getSynthesizer();
 			synth.open();
 			
 			MidiChannel[] m_channel = synth.getChannels();
-			System.out.println("Number of Channels: " + m_channel.length);
+			Instrument[] m_instr = synth.getAvailableInstruments();
+			
+			for(Instrument i : m_instr)
+				System.out.println(i);
+//			
+			
+//			System.out.println("Number of Channels: " + m_channel.length);
+//			
+//			Receiver rec = synth.getReceiver();
+//			
+//			rec.send(new ShortMessage(ShortMessage.PROGRAM_CHANGE, 0, 109, 0), -1);
+			
+			
+			System.out.println(m_channel[0].getController(0));
+			
+			change_instrument(0, 640, 124, synth);
+			
 			
 			Integer x = 60;
-			for (Integer i = 0; i < 10; i++) {
-				m_channel[0].noteOn(test.note_mapper.get("C6"), 100);
+			for (Integer i = 0; i < 1; i++) {
+				m_channel[0].noteOn(test.note_mapper.get("C4"), 1000);
 				x += 5;
-				Thread.sleep(1000);
+				Thread.sleep(5000);
 			}
 
 			
